8baf4d9770c17122bfe6fcb23812c5dc8aa54959
I-GIKARU <aizohgikaru@gmail.com>
2025-02-21 21:25:57 +0300

    Implemented missing functions

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+// 1️⃣ Function to generate thank-you messages
+function writeCards(names, event) {
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+}
+
+// 2️⃣ Function to count down from a given number to 0
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
